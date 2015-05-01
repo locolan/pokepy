cd8cbd676e0fa6f1a4c7191903ec7232ffb94206
locolan <lochlan.hehr@yahoo.com>
2015-05-01 09:47:41 -0500

    important view changes

--- pokepy/views.py
+++ pokepy/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return HttpResponse("Welcome to the internet! Suck my cock!")
     
 def admin(request):
-    return HttpResponse("Hello, world. You're at the admin.")
+    return HttpResponse("Hello, world. You're the admin.")
     
 def search(request):
-    return HttpResponse("Hello, world. You're at the search.")
+    return HttpResponse("Hello, consumer. You are the the search.")
