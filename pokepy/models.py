b46ca3ada67cf881eda78db40075b62b8d7d05b9
locolan <lochlan.hehr@yahoo.com>
2015-05-02 17:29:51 -0500

    database population in progress.

--- pokepy/models.py
+++ pokepy/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
 class Search(models.Model):
-    def __init__(self):
+    def __init__(self,query_text):
         self.query_text = models.CharField(max_length=50,default='default')
     
     def __str__(self):
         return self.query_text
     
 class EggGroups(models.Model):
-    def __init__(self):
+    def __init__(self,egg_group):
         self.egg_group = models.CharField(max_length=50,default='default')
     def __str__(self):
         return self.egg_group
     
 class Moves(models.Model):
-    def __init__(self):
+    def __init__(self,move,type):
         self.move = models.CharField(max_length=50,default='default')
         self.type = models.CharField(max_length=50,default='default')
     def __str__(self):
@@ -22,13 +22,13 @@ class Moves(models.Model):
     
     
 class Abilities(models.Model):
-    def __init__(self):
+    def __init__(self,ability):
         self.ability = models.CharField(max_length=50,default='default')
     def __str__(self):
         return self.ability
     
 class Pokemon(models.Model):
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.name = models.CharField(max_length=50, default='bob')
         self.type1 = models.CharField(max_length=50,default='normal')
         self.type2 = models.CharField(max_length=50,default='normal')
--- pokepy/views.py
+++ pokepy/views.py
@@ -5,9 +5,15 @@ from django.conf import settings
 from django import forms
 from .forms import SearchForm
 from importlib import import_module
-import pykemon
+import pykemon,simplejson
+from pykemon import api,request,models
 
 from .models import Pokemon
+
+
+def poke_to_json(poke):
+    return simplejson.JSONEncoder(poke)
+    
 
 
 def valid_address(request,form):
@@ -16,7 +22,33 @@ def valid_address(request,form):
         r = form.cleaned_data['search_query'] 
         print(r)
         try:
-            request.session['poke'] = pykemon.get(pokemon=r)
+            poke = pykemon.get(pokemon=r)
+            poke_moves = []
+            poke_abils = []
+            for f in poke.moves:
+                poke_moves.append(f)
+            for f in poke.abilities:
+                poke_abils.append(f)
+            try:
+                p = Pokemon(name=poke.name,
+                            type1 = poke.types.popitem(), type2 = poke.types.popitem(),
+                            games=poke.games, description=poke.description,
+                            egg_group=poke.egg_groups, moves=poke_moves,
+                            abilities=poke_abils,
+                            )
+                p.save()
+            except KeyError:
+                pass
+            
+#             print(poke.name + " conversion to JSON")
+#             
+#             mylist = list(poke.get_queryset().values_list('code', flat=True))
+#             print("mark 1")
+#             json_data = json.dumps(mylist)
+#             print("mark 2")
+#             response = HttpResponse(json_data, content_type='application/json')
+#             print("mark 3")
+            request.session['poke'] = poke.id
         except(pykemon.ResourceNotFoundError):
             print("Why does this keep happening?")
             pass
@@ -55,8 +87,8 @@ def index(request):
 
 
 def search(request): 
-    request.session['poke'] = None
-     
+    request.session['poke'] = 0
+    poke = "" 
     form = SearchForm(request.POST)
     template = loader.get_template('pokepy/search.html')
     
@@ -77,11 +109,26 @@ def search(request):
         egg_group = list(poke.egg_groups.keys())[0]
         form = SearchForm()
         
+        
+        
+#         print(poke.name + " conversion to JSON")
+#         
+#         mylist = list(poke.get_queryset().values_list('code', flat=True))
+#         print("mark 1")
+#         json_data = json.dumps(mylist)
+#         print("mark 2")
+#         response = HttpResponse(json_data, content_type='application/json')
+#         print("mark 3")
+#         print("compiling context")
+        
+        
         context = RequestContext(request, {
             'move': move,'egg_group': egg_group,
-            'form': form,
+            'form': form,'poke': poke,
         })
-        print(poke.name + " , " + poke.weight)
+        
+        print("END")
+        
         return HttpResponse(template.render(context))
         
     except AttributeError:
